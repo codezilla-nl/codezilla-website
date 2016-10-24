af50dfc4eee8129bebc8b637719ab372cf038fdb
Jelle Kralt <jelle@jellekralt.nl>
2016-10-24 22:43:07 +0200

    Added browser reload

--- bs-config.dev.js
+++ bs-config.dev.js
@@ -3,16 +3,26 @@ module.exports = {
     "ui": {
         port: 3001
     },
-    "files": [
-        "content/*.md",
-        "src/*.js",
-        "src/*.html",
-        "src/*.scss"
-    ],
+    "files": [{
+        match: [
+            "./content/**/*.md",
+            "./src/**/*.js",
+            "./src/**/*.html",
+            "./src/**/*.scss"
+        ],
+        fn: function (event, file) {
+            if (event === 'change') {
+                require('./index')(function() {
+                    this.reload();
+                }.bind(this));
+            }
+        }
+    }],
     "watchOptions": {},
     "server": {
         baseDir: "build"
     },
     "open": true,
-    "notify": false
+    "notify": false,
+    "browser": process.env.DEV_BROWSER || 'default'
 };
