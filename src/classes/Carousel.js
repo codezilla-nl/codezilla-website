47292e352c3d14b296fe61aa92fdb4c942e05be1
Huib van Geertruy <hvgeertruy@gmail.com>
2017-01-11 23:06:10 +0100

    feat: built header animation

--- src/classes/Carousel.js
+++ src/classes/Carousel.js
@@ -1,23 +1,64 @@
-export default class Carousel {
+export default class {
     constructor(instance) {
-        if (!isElement(instance)) {
-            console.error(`Carousel ${instance} is not a valid HTML element`);
+        const el = document.querySelector(instance);
+        this.cycle = undefined;
+        this.slides = el.children;
+        this.options = {
+            interval: 6000,
+            class: {
+                current: 'cz-carousel--current',
+                transition: 'cz-carousel--transition'
+            }
+        };
+
+        if (!el.nodeName) {
+            console.error(`Carousel ${el} is not a valid HTML element`);
             return;
         }
         this.carousel = instance;
         
-        console.info('aaa', instance);
+        //Show First slide
+        this.slides[0].classList.add(this.options.class.current);
+        this.init();
     }
 
+    init() {
+        var _self = this;
+        this.cycle = setInterval(function() { _self.next() }, _self.options.interval);
+    }
+
+    next() {
+        let current, next;
+        for (let i = 0; i < this.slides.length; i++) {
+            if (this.slides[i].classList.value.indexOf(this.options.class.current) > -1) {
+                current = this.slides[i];
+                next = (i + 1 < this.slides.length) ? this.slides[i+1] : this.slides[0];
+            }
+        }
+
+        current.classList.add(this.options.class.transition);
+
+        //on transitionend
+        let _self = this;
+        setTimeout(function() {
+            current.classList.remove(_self.options.class.transition);
+            current.classList.remove(_self.options.class.current);
+            next.classList.add(_self.options.class.current);
+        }, 1600);
+
+    }
+
+    //get from rabo
+    // el.addEventListener('webkitTransitionEnd', vm.transitionEnd(someEl), false);
+
+    // };
+
+    // vm.transitionEnd = function(somEl) {
+    //     angular.element(somEl).removeClass('animating'); 
+    // };
 
     // set bar(value) {
     //     this.bar = value;
     // }
 
-    isElement(o) {
-        return (
-            typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
-            o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string"
-        );
-    }
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Foo from './classes/Foo.js'
 import Triangles from './classes/Triangles.js'
+import Carousel from './classes/Carousel.js'
 
 class Main {
     constructor() {
         // new Foo();
-        const triangles = new Triangles('#triangles');
+        const triangles = new Triangles('[cz-triangles]');
         triangles.addGlobalListeners(); // TODO: this could be nicer
 
+        const carousel = new Carousel('[cz-carousel]');
     }
 }
 
