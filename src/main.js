e6dfbfec5a7eaef55c0c1093ab82d53e5481f5d8
Jelle Kralt <jelle@jellekralt.nl>
2017-01-20 12:53:58 +0100

    Removed triangles example from js

--- src/main.js
+++ src/main.js
@@ -4,11 +4,8 @@ import Triangles from './classes/Triangles.js'
 class Main {
     constructor() {
         // new Foo();
-        const triangles = new Triangles('#triangles');
         const footer = new Triangles('.cz-footer__triangles');
-        triangles.addGlobalListeners(); // TODO: this could be nicer
         footer.addGlobalListeners(); // TODO: this could be nicer
-
     }
 }
 
