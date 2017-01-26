60abe5c1304d4ecbbdaf74855241f7c28ca2e1bb
Huib van Geertruy <hvgeertruy@gmail.com>
2017-01-26 20:38:12 +0100

    fix: readded classname for Triangles
    - review comment
    - will name class in case of error

--- src/classes/Triangles.js
+++ src/classes/Triangles.js
@@ -1,7 +1,7 @@
 import UtilInViewport from "../utils/util.inViewport.js";
 import UtilRegisterScroll from "../utils/util.registerScroll.js";
 
-export default class {
+export default Triangles class {
     constructor(el) {
         this.options = {
             canvas: document.querySelector(el),
